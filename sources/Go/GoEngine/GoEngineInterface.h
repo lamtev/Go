01e846f80f306590eb145511b441f2f22ab266b0
antonlamtev <antonlamtev@gmail.com>
2016-03-21 01:43:25 +0300

    edit GoEngineInterface. document it

--- sources/Go/GoEngine/GoEngineInterface.h
+++ sources/Go/GoEngine/GoEngineInterface.h
@@ -1,22 +1,28 @@
 #ifndef GOENGINEINTERFACE_H
 #define GOENGINEINTERFACE_H
 
+#include <string>
 #include "enums.h"
-
 /**
  * @brief Класс, предоставляющий интерфейс движка приложению
  */
 class GoEngineInterface
 {
 public:
-    ///Начать новую игру
-    void startNewGame();
-    ///Загрузить игру
-    void loadGame();
+
+    /**
+     * @brief Начать игру.
+     *
+     * Метод, позволяющий начать игру в соответствии с параметрами, которые задаются его аргументами.
+     * @param config Конфиг
+     */
+    void startGame(std::string config=0, int size=0, Rules rules=JAPANESE, bool isNigiri=true);
     ///Положить камень в выбранный пункт
     void putStone(PlayerColor color, LiteralCoordinate x, int y);
     ///Пропустить ход
-    void check(PlayerColor color);
+    void pass(PlayerColor color);
+    ///Cдаться
+    void resign(PlayerColor color);
 
 private:
 
--- sources/Go/GoEngine/enums.h
+++ sources/Go/GoEngine/enums.h
@@ -7,4 +7,6 @@ enum PlayerColor {BLACK = 0, WHITE};
 ///Буквенная координата
 enum LiteralCoordinate {A = 1, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S};
 
+enum Rules {JAPANESE = 0, CHINES};
+
 #endif // ENUMS_H
