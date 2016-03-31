db134dea8039063ff0320b069b72f6a236f154df
antonlamtev <antonlamtev@gmail.com>
2016-03-31 22:43:47 +0300

    add build.sh, edit cmake

---
+++ build.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+export PATH=$PATH:/opt/Qt5.5.0/5.5/gcc_64/bin/
+
+buildReleaseVersion() {
+	mkdir build 
+	cd build
+	mkdir release
+	cd release
+	cmake --version
+    cmake –G "Unix Makefiles" ../../sources
+
+	if [ -e "Makefile" ]; then
+		cmake --build ./ --target GoCUI --
+		cd ../..
+	else
+	    cd ../..
+		echo "Makefile does not exist"
+		echo "Build of release version failed!"
+		exit 1
+	fi
+	ls
+}
+
+buildDebugVersion() {
+    cd sources
+    cloc --version
+	cloc --by-file --xml --out=../report/cloc/log *
+	cd ../build
+	mkdir debug
+	cd debug
+	cmake --version
+	cmake –G "Unix Makefiles" ../../
+	if [ -e "Makefile" ]; then
+		cmake --build ./ --clean-first --
+
+		cd ../../sources
+		../build/debug/GoTests/FunctionalTest/FTest -xml -o ../report/tests/functional/log || true
+		#../build/debug/GoTests/ModuleTests/name -xml -o ../report/tests/module/name/log || true
+
+		cppcheck --version
+		cppcheck --enable=all -v  --xml  * 2> ../report/cppcheck/log
+
+        cd ../build/debug/GoEngine/CMakeFiles/GoEngine.dir
+        ls
+		#gcovr --version
+		#gcovr -r . --xml --exclude='' -o ../../../../../report/gcovr/log
+
+		cd ../../../../../sources
+		ls
+		valgrind --version
+		valgrind --leak-check=full --xml=yes --xml-file=/opt/tomcat/.jenkins/jobs/Go/workspace/report/valgrind/functionalTest/FTest.%p /opt/tomcat/.jenkins/jobs/Go/workspace/build/debug/GoTests/FunctionalTest/FTest || true
+		
+
+        cd ../report/doxygen
+		if [ -e "goconfig" ]; then
+			doxygen --version
+			doxygen goconfig
+		else
+			echo "Doxygen failed"
+			echo "goconfig does not exist"
+		fi
+
+		cd ../..
+	else
+	    cd ..
+		echo "Makefile does not exist"
+		echo "Build of debug version failed!"
+		exit 1
+	fi
+}
+
+makeReport() {
+	cd report/doxygen/
+	ls
+	if [ -e "goconfig" ]; then
+			doxygen --version
+			doxygen goconfig
+		else
+			echo "Doxygen failed"
+			echo "goconfig does not exist"
+	fi
+	cd latex
+	if [ -e "Makefile" ]; then
+		make --version
+		make
+		cd ../../..
+	else
+		echo "Makefile does not exist"
+		echo "Report failure!"
+		cd ../../..
+		exit 1
+	fi
+}
+
+zip_files() {
+    ls
+	if [ -z ${JOB_NAME} ] || [ -z ${BUILD_NUMBER}]; then
+		echo "Vars JOB_NAME/BUILD_NUMBER are unset"
+		echo "Zip failure!"
+		exit 1
+	fi
+
+	TITLE="${JOB_NAME}_v${BUILD_NUMBER}"
+	mkdir "$TITLE"
+
+	if [ -e "build/release/GoCUI/GoCUI" ]; then
+		cp build/release/GoCUI/GoCUI $TITLE/Go_v${BUILD_NUMBER}
+		if [ -e "report/doxygen/latex/refman.pdf" ]; then
+			cp report/doxygen/latex/refman.pdf $TITLE/Go_v${BUILD_NUMBER}.pdf
+		fi
+		zip --version
+		zip $TITLE.zip $TITLE/*
+	else
+		echo "GoCUI does not exist"
+		echo "Zip failure!"
+		exit 1
+	fi
+
+}
+
+clean() {
+	git clean -f -e *.zip *.pdf
+}
--- sources/GoEngine/enums.h
+++ sources/GoEngine/enums.h
@@ -1,7 +1,7 @@
 #ifndef ENUMS_H
 #define ENUMS_H
 
-enum Color {BLACK = 0, WHITE};
+enum Color {BLACK = 1, WHITE};
 
 enum LiteralCoordinate {A = 1, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S};
 
