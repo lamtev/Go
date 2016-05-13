5f66ed99ca52ef81aa8af783db0701f3bb63d807
antonlamtev <antonlamtev@gmail.com>
2016-05-13 21:46:18 +0300

    add QString

--- sources/GoGUI/main.cpp
+++ sources/GoGUI/main.cpp
@@ -1,19 +1,20 @@
 #include <QtWidgets>
 #include <QVector>
+#include <QString>
 
 int main( int argc, char** argv )
 {
     QApplication goGUI{ argc, argv };
-    const QVector<const char*> vec{
+    const QVector<QString> vec{
         "GUI is almost done!",
         "AHAHAHAHA",
         "Go"
     };
 
-    QLabel label1{ vec[0] };
+    QLabel label1{ vec[1] };
     label1.show();
 
-    QLabel label2{ vec[1] };
+    QLabel label2{ vec[0] };
     label2.show();
 
     QLabel label3{ vec[2] };
