d8725f8955d3f1403173f79de3502e9326a8c217
antonlamtev <antonlamtev@gmail.com>
2016-05-13 21:40:51 +0300

    add QVector

--- sources/GoGUI/main.cpp
+++ sources/GoGUI/main.cpp
@@ -1,11 +1,23 @@
 #include <QtWidgets>
+#include <QVector>
 
 int main( int argc, char** argv )
 {
     QApplication goGUI{ argc, argv };
-    QLabel label{ "GoGUI" };
+    const QVector<const char*> vec{
+        "GUI is almost done!",
+        "AHAHAHAHA",
+        "Go"
+    };
 
-    label.show();
+    QLabel label1{ vec[0] };
+    label1.show();
+
+    QLabel label2{ vec[1] };
+    label2.show();
+
+    QLabel label3{ vec[2] };
+    label3.show();
 
     return goGUI.exec();
 }
