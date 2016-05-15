2bc59ad5d98a550459d853790ba879513080583a
antonlamtev <antonlamtev@gmail.com>
2016-05-15 13:46:30 +0300

    QBoxLayout -> QHBoxLayout

--- sources/GoGUI/main.cpp
+++ sources/GoGUI/main.cpp
@@ -17,14 +17,15 @@ int main( int argc, char** argv )
     QPushButton* pcmdSettings = new QPushButton{ "Settings" };
     QPushButton* pcmdExit = new QPushButton{ "Exit" };
 
-    QBoxLayout* pbxLayout = new QBoxLayout{ QBoxLayout::TopToBottom };
-    pbxLayout->addWidget(pcmdStartGame, 1);
-    pbxLayout->addWidget(pcmdHelp, 2);
-    pbxLayout->addWidget(pcmdSettings, 3);
-    pbxLayout->addWidget(pcmdExit, 4);
-    wgt.setLayout(pbxLayout);
-
-    wgt.resize(300, 200);
+    QHBoxLayout* pbhxLayout = new QHBoxLayout;
+    pbhxLayout->setMargin(10);
+    pbhxLayout->setSpacing(20);
+    pbhxLayout->addWidget(pcmdStartGame);
+    pbhxLayout->addWidget(pcmdHelp);
+    pbhxLayout->addWidget(pcmdSettings);
+    pbhxLayout->addWidget(pcmdExit);
+    wgt.setLayout(pbhxLayout);
+    
     wgt.show();
 
     qDebug() <<  goGUI.metaObject()->superClass()->superClass()->superClass()->className();
