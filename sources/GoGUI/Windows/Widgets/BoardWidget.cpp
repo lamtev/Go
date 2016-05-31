2c66716355ab77858548e4123ecfa0fe2b19e182
antonlamtev <antonlamtev@gmail.com>
2016-05-31 22:13:09 +0300

    add simple stone drawing

--- sources/GoGUI/Windows/Widgets/BoardWidget.cpp
+++ sources/GoGUI/Windows/Widgets/BoardWidget.cpp
@@ -2,15 +2,27 @@
 
 BoardWidget::BoardWidget(const int BOARD_DIAG, QWidget *parent) noexcept :
     QFrame{parent},
+    BACKGROUND{QPixmap{":/board_background2.jpg"}},
     BOARD_DIAG{BOARD_DIAG},
-    BOARD_HEIGHT{QImage{":/board_background2.jpg"}.height()},
+    BOARD_HEIGHT{BACKGROUND.height()},
     POINT_HEIGHT{BOARD_HEIGHT/(BOARD_DIAG + 1)},
     DELTA{BOARD_HEIGHT - (BOARD_HEIGHT/(BOARD_DIAG + 1))*(BOARD_DIAG + 1)},
-    ACTIVE_BOARD_LEFT{QPixmap{":/board_background2.jpg"}.rect().left() + 3*POINT_HEIGHT/4},
-    ACTIVE_BOARD_TOP{QPixmap{":/board_background2.jpg"}.rect().top() + 3*POINT_HEIGHT/4},
-    ACTIVE_BOARD_RIGHT{QPixmap{":/board_background2.jpg"}.rect().right() - 3*POINT_HEIGHT/2},
-    ACTIVE_BOARD_BOTTOM{QPixmap{":/board_background2.jpg"}.rect().bottom() - 3*POINT_HEIGHT/2},
-    ACTIVE_BOARD_RECT{ACTIVE_BOARD_LEFT, ACTIVE_BOARD_TOP, ACTIVE_BOARD_RIGHT, ACTIVE_BOARD_BOTTOM},
+    ACTIVE_BOARD_LEFT{BACKGROUND.rect().left() + POINT_HEIGHT},
+    ACTIVE_BOARD_TOP{BACKGROUND.rect().top() + POINT_HEIGHT},
+    ACTIVE_BOARD_WIDTH{BACKGROUND.width() - 2*POINT_HEIGHT},
+    ACTIVE_BOARD_HEIGHT{BACKGROUND.height() - 2*POINT_HEIGHT},
+    ACTIVE_BOARD_RECT{ACTIVE_BOARD_LEFT,
+                      ACTIVE_BOARD_TOP,
+                      ACTIVE_BOARD_WIDTH,
+                      ACTIVE_BOARD_HEIGHT},
+    ACTIVE_ZONE_LEFT{ACTIVE_BOARD_LEFT - POINT_HEIGHT/4},
+    ACTIVE_ZONE_TOP{ACTIVE_BOARD_TOP - POINT_HEIGHT/4},
+    ACTIVE_ZONE_WIDTH{ACTIVE_BOARD_WIDTH + 2*POINT_HEIGHT/4},
+    ACTIVE_ZONE_HEIGHT{ACTIVE_BOARD_HEIGHT + 2*POINT_HEIGHT/4},
+    ACTIVE_ZONE_RECT{ACTIVE_ZONE_LEFT,
+                     ACTIVE_ZONE_TOP,
+                     ACTIVE_ZONE_WIDTH,
+                     ACTIVE_ZONE_HEIGHT},
     BOARD_RECT{QPixmap{":/board_background2.jpg"}.rect()},
     isGoConfigured{false},
     go{nullptr} {
@@ -42,6 +54,7 @@ void BoardWidget::paintEvent(QPaintEvent *paintEvent) noexcept {
   painter.setRenderHint(QPainter::Antialiasing);
 
   drawBoard(painter);
+  drawStones(painter);
 }
 
 void BoardWidget::update() noexcept {
@@ -50,7 +63,7 @@ void BoardWidget::update() noexcept {
 }
 
 void BoardWidget::mousePressEvent(QMouseEvent *mouseEvent) {
-  if(ACTIVE_BOARD_RECT.contains(mouseEvent->pos()))
+  if(ACTIVE_ZONE_RECT.contains(mouseEvent->pos()))
   {
     qDebug() << "nice!!!";
     int first = determinePointCoordinates(mouseEvent->pos()).x();
@@ -77,8 +90,36 @@ void BoardWidget::drawBoard(QPainter &painter) const noexcept {
   }
 }
 
+void BoardWidget::drawStones(QPainter &painter) noexcept {
+  //pointsRects.resize(BOARD_DIAG*BOARD_DIAG);
+
+//  for (auto rect : pointsRects) {
+//    rect = QRect{};
+//  }
+  drawStone(painter, "black");
+}
+
 void BoardWidget::drawStone(QPainter &painter, const QString& color) const noexcept {
-  painter.drawImage(BOARD_RECT, QImage{":/" + color + "_stone_" + QString{BOARD_DIAG}});
+  const char *diagonal;
+  switch (BOARD_DIAG) {
+    case 19 :
+      diagonal = "19";
+      break;
+    case 13 :
+      diagonal = "13";
+      break;
+    case 7 :
+      diagonal = "7";
+      break;
+    default:
+      qDebug() << "Wrong diagonal";
+  }
+  QRect rect{ACTIVE_BOARD_LEFT - POINT_HEIGHT/2,
+             ACTIVE_BOARD_TOP - POINT_HEIGHT/2,
+             POINT_HEIGHT,
+             POINT_HEIGHT};
+  //painter.drawImage(rect, QImage{":/" + color + "_stone_" + diagonal + ".png"});
+  painter.drawImage(rect, QImage{":/white_stone.png"});
 }
 
 QPoint BoardWidget::determinePointCoordinates(const QPoint &qPoint) const noexcept {
--- sources/GoGUI/Windows/Widgets/BoardWidget.h
+++ sources/GoGUI/Windows/Widgets/BoardWidget.h
@@ -28,23 +28,30 @@ class BoardWidget : public QFrame {
   void mousePressEvent(QMouseEvent *mouseEvent) override;
 
  private:
+  const QPixmap BACKGROUND;
   const int BOARD_DIAG;
   const int BOARD_HEIGHT;
   const int POINT_HEIGHT;
   const int DELTA;
   const int ACTIVE_BOARD_LEFT;
   const int ACTIVE_BOARD_TOP;
-  const int ACTIVE_BOARD_RIGHT;
-  const int ACTIVE_BOARD_BOTTOM;
+  const int ACTIVE_BOARD_WIDTH;
+  const int ACTIVE_BOARD_HEIGHT;
   const QRect ACTIVE_BOARD_RECT;
+  const int ACTIVE_ZONE_LEFT;
+  const int ACTIVE_ZONE_TOP;
+  const int ACTIVE_ZONE_WIDTH;
+  const int ACTIVE_ZONE_HEIGHT;
+  const QRect ACTIVE_ZONE_RECT;
   const QRect BOARD_RECT;
-  //const QRect
+  QVector<QRect> pointsRects;
 
   bool isGoConfigured;
 
   GoEngineAPI *go;
 
   void drawBoard(QPainter &painter) const noexcept;
+  void drawStones(QPainter &painter) noexcept;
   void drawStone(QPainter &painter, const QString& color) const noexcept;
   QPoint determinePointCoordinates(const QPoint &qPoint) const noexcept;
   void configureGo() noexcept;
